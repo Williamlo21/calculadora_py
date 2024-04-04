abb055c7142d84117104a8bec2812ae3e73e303c
Williamlo21 <lopezrebellonw@gmail.com>
2024-04-04 10:47:44 -0500

    calculador en python

---
+++ calculadora.py
@@ -0,0 +1,44 @@
+def sumar(n1, n2):
+    print("Resultado de la suma:", n1 + n2)
+
+def restar(n1, n2):
+    print("Resultado de la resta:", n1 - n2)
+
+def multiplicar(n1, n2):
+    print("Resultado de la multiplicación:", n1 * n2)
+
+def dividir(n1, n2):
+    if n2 == 0:
+        print("Error: No se puede dividir por cero")
+    else:
+        print("Resultado de la división:", n1 / n2)
+
+def buscarOperacion(operacion, n1, n2):
+    opciones = {
+        's': sumar,
+        'r': restar,
+        'm': multiplicar,
+        'd': dividir,
+    }
+    if operacion in opciones:
+        opciones[operacion](float(n1), float(n2))
+    else:
+        print("Operación no disponible.")
+
+print("Esta es una calculadora")
+print("Por favor indica qué operación deseas realizar.")
+print("Escribe 's' si es suma, 'r' si es resta, 'm' si es multiplicación y 'd' si es división:")
+operacion = input()
+while operacion != 's' and operacion != 'r' and operacion != 'm' and operacion != 'd':
+    print("La operación " + operacion + " no está disponible.")
+    print("Por favor, indique una operación disponible.")
+    operacion = input()
+
+
+
+print("Escogiste la opción:", operacion)
+print("Ahora digita el primer número para la operación:")
+n1 = input()
+print("Ahora el segundo número:")
+n2 = input()
+buscarOperacion(operacion, n1, n2)
